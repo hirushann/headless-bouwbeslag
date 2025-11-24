601058279cbf4e35adccd64f901744ab1d78d026
Hirushan Perera <kir6rzv1qm6ek8mdynqpma9mkmtp5p@bots.bitbucket.org>
2025-11-24 12:21:23 +0530

    fix

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -1,7 +1,7 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 const api = new WooCommerceRestApi({
-  url: process.env.NEXT_PUBLIC_WORDPRESS_API_URL as string,
+  url: process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "https://dayzsolutions.com/empire",
   consumerKey: process.env.NEXT_PUBLIC_WC_CONSUMER_KEY as string,
   consumerSecret: process.env.NEXT_PUBLIC_WC_CONSUMER_SECRET as string,
   version: "wc/v3",
