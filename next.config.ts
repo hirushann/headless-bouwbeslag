91036582137e875f0f840190eb740f243d9c99b3
Hirushan Perera <kir6rzv1qm6ek8mdynqpma9mkmtp5p@bots.bitbucket.org>
2025-11-24 13:19:26 +0530

    changes after putting to live 2

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "staging-plugin-test.test",
       },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
