8854646a1f42a5d266e81b68e1652cd33f79629a
Hirushan Perera <hirushanperera@Hirushans-MacBook-Air.local>
2026-06-11 14:56:37 +0530

    google tag manager code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,20 @@ export default async function RootLayout({
   return (
     <html lang="nl" data-theme="light">
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NBGNBVR3');
+            `,
+          }}
+        />
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-F21GZC6NGG"
@@ -64,6 +78,15 @@ export default async function RootLayout({
         />
       </head>
       <body className={`${dmsans.variable} ${geistMono.variable} font-sans antialiased overflow-visible`} >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NBGNBVR3"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Toaster position="top-right" />
         <UserProvider>
           <ProductAddedModalProvider>
